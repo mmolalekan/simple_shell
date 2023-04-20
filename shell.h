28ea2933f410e8acaabd458b8360e3b3f7609ef9
mire-web <amahe8664@gmail.com>
2023-04-20 13:29:57 +0100

    Fix Betty error

--- shell.h
+++ shell.h
@@ -13,6 +13,11 @@ extern char **environ;
 
 /*UTILS*/
 int _putchar(char c);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+int _atoi(char *s);
 
 /*MISCELLANEOUS*/
 
