59edebe092aa5097f98b580e1bdea5b092de694a
mire-web <amahe8664@gmail.com>
2023-04-20 12:20:12 +0100

    Add Header file

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/*Standard Libraries*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/*EXTERNS*/
+extern char **environ;
+
+/*CORE*/
+
+/*UTILS*/
+int _putchar(char c);
+
+/*MISCELLANEOUS*/
+
+#endif
