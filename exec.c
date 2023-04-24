7f6e32a2da5d37d3deeebbf100bbfbc4618ba4e9
Mire <amahe8664@gmail.com>
2023-04-24 21:15:56 +0100

    Fix Betty error

--- exec.c
+++ exec.c
@@ -47,7 +47,7 @@ char **tokenize(char *lineptr, char *delim)
 int execute(const char *pathname, char *const argv[], char *const env[])
 {
 	pid_t pid;
-	int n, i;
+	int n;
 
 	if ((argv[0][0] == '/') == 1 || argv[0][0] == '.')
 	{
@@ -72,30 +72,43 @@ int execute(const char *pathname, char *const argv[], char *const env[])
 	}
 	else
 	{
-		char *filepath;
-		char __attribute__((unused)) *new_argv[1024 * sizeof(char)];
-
-		filepath = search_path(argv[0]);
-		/*for (i = 0; )*/
-		if (filepath)
-		{
-			new_argv[0] = filepath;
-			i = 1;
-			while (argv[i])
-			{
-				new_argv[i] = NULL;
-				new_argv[i] = argv[i];
-				printf("%s\n", new_argv[i]);
-				i++;
-			}
-			execute(new_argv[0], new_argv, env);
-		}
-		else
-		{
-			write(STDERR_FILENO, pathname, _strlen(pathname));
-			write(STDERR_FILENO, ": not found", 12);
-			_putchar('\n');
-		}
+		handle_relative_path(argv, env);
 	}
 	return (0);
+}
+
+/**
+ * handle_relative_path - Handle relative path for executables
+ *
+ * @argv: argument vector list
+ * @env: enviroment variables
+ */
+
+void handle_relative_path(char *const argv[], char *const env[])
+{
+	char *filepath;
+	int i;
+
+	char __attribute__((unused)) *new_argv[1024 * sizeof(char)];
+	filepath = search_path(argv[0]);
+	/*for (i = 0; )*/
+	if (filepath)
+	{
+		for (i = 0; new_argv[i]; i++)
+			new_argv[i] = '\0';
+		new_argv[0] = filepath;
+		i = 1;
+		while (argv[i])
+		{
+			new_argv[i] = argv[i];
+			i++;
+		}
+		execute(new_argv[0], new_argv, env);
+	}
+	else
+	{
+		write(STDERR_FILENO, argv[0], _strlen(argv[0]));
+		write(STDERR_FILENO, ": not found", 12);
+		_putchar('\n');
+	}
 }
